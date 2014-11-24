6f2a88e8e4defae359c18df125ea355e3d21fa2b
Wesley Shields <wxs@atarininja.org>
2014-11-24 00:03:27 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "pycrits",
+    version = "0.1",
+    author = "Wesley Shields",
+    author_email = "wxs@atarininja.org",
+    description = ("Python interface to CRITs API."),
+    license = "BSD",
+    keywords = "CRITs",
+    url = "https://github.com/wxsBSD/pycrits",
+    packages=['pycrits'],
+    long_description="Python interface to the CRITs API."
+)
