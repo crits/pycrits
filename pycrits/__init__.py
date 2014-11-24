9d675c13c824335339c92abacfda9b439f6520f2
Wesley Shields <wxs@atarininja.org>
2014-11-24 12:49:06 -0500

    Add counts and refactor.
    You can now use sample_count() and friends to get a total number of given
    objects available. You can pass arbitrary search parameters too.
    
    Refactor the code so that the generator and single fetch both use the same
    function to actually fetch.

--- pycrits/__init__.py
+++ pycrits/__init__.py
@@ -74,21 +74,35 @@ class pycrits(object):
     def verify(self, value):
         self._verify = bool(value)
 
-    def _fetch(self, url, params={}):
+    # Actually do the fetching.
+    def _do_fetch(self, url, params={}):
+        resp = requests.get(url, params=params, verify=self._verify)
+        if resp.status_code != 200:
+            raise pycritsFetchError("Response code: %s" % resp.status_code)
+
+        try:
+            results = json.loads(resp.text)
+        except:
+            raise pycritsFetchError("Unable to load JSON.")
+
+        return results
+
+    # Fetch and return once.
+    def _single_fetch(self, url, params={}):
+        params['username'] = self._username
+        params['api_key'] = self._api_key
+        url = self._base_url + url
+        return self._do_fetch(url, params=params)
+
+    # Fetch and yield a generator. Iterations will continue to fetch.
+    def _fetch_generator(self, url, params={}):
         params['username'] = self._username
         params['api_key'] = self._api_key
         url = self._base_url + url
 
         next_ = True
         while next_:
-            resp = requests.get(url, params=params, verify=self._verify)
-            if resp.status_code != 200:
-                raise pycritsFetchError("Response code: %s" % resp.status_code)
-
-            try:
-                results = json.loads(resp.text)
-            except:
-                raise pycritsFetchError("Unable to load JSON.")
+            results = self._do_fetch(url, params)
             yield results
             next_ = results['meta']['next']
             if next_:
@@ -96,46 +110,95 @@ class pycrits(object):
                 params={}
 
     def indicators(self, params={}):
-        return self._fetch(self._INDICATORS, params)
+        return self._fetch_generator(self._INDICATORS, params)
 
     def actors(self, params={}):
-        return self._fetch(self._ACTORS, params)
+        return self._fetch_generator(self._ACTORS, params)
 
     def actor_identifiers(self, params={}):
-        return self._fetch(self._ACTOR_IDENTIFIERS, params)
+        return self._fetch_generator(self._ACTOR_IDENTIFIERS, params)
 
     def campaigns(self, params={}):
-        return self._fetch(self._CAMPAIGNS, params)
+        return self._fetch_generator(self._CAMPAIGNS, params)
 
     def certificates(self, params={}):
-        return self._fetch(self._CERTIFICATES, params)
+        return self._fetch_generator(self._CERTIFICATES, params)
 
     def domains(self, params={}):
-        return self._fetch(self._DOMAINS, params)
+        return self._fetch_generator(self._DOMAINS, params)
 
     def emails(self, params={}):
-        return self._fetch(self._EMAILS, params)
+        return self._fetch_generator(self._EMAILS, params)
 
     def events(self, params={}):
-        return self._fetch(self._EVENTS, params)
+        return self._fetch_generator(self._EVENTS, params)
 
     def indicators(self, params={}):
-        return self._fetch(self._INDICATORS, params)
+        return self._fetch_generator(self._INDICATORS, params)
 
     def pcaps(self, params={}):
-        return self._fetch(self._PCAPS, params)
+        return self._fetch_generator(self._PCAPS, params)
 
     def raw_data(self, params={}):
-        return self._fetch(self._RAW_DATA, params)
+        return self._fetch_generator(self._RAW_DATA, params)
 
     def samples(self, params={}):
-        return self._fetch(self._SAMPLES, params)
+        return self._fetch_generator(self._SAMPLES, params)
 
     def screenshots(self, params={}):
-        return self._fetch(self._SCREENSHOTS, params)
+        return self._fetch_generator(self._SCREENSHOTS, params)
 
     def targets(self, params={}):
-        return self._fetch(self._TARGETS, params)
+        return self._fetch_generator(self._TARGETS, params)
+
+    # Force limit to 1 and only return _id.
+    def _fetch_count(self, url, params={}):
+        params['limit'] = 1
+        params['only'] = 'id'
+        results = self._single_fetch(url, params)
+        return results['meta']['total_count']
+
+    def indicator_count(self, params={}):
+        return self._fetch_count(self._INDICATORS, params)
+
+    def actor_count(self, params={}):
+        return self._fetch_count(self._ACTORS, params)
+
+    def actor_identifier_count(self, params={}):
+        return self._fetch_count(self._ACTOR_IDENTIFIERS, params)
+
+    def campaign_count(self, params={}):
+        return self._fetch_count(self._CAMPAIGNS, params)
+
+    def certificate_count(self, params={}):
+        return self._fetch_count(self._CERTIFICATES, params)
+
+    def domain_count(self, params={}):
+        return self._fetch_count(self._DOMAINS, params)
+
+    def email_count(self, params={}):
+        return self._fetch_count(self._EMAILS, params)
+
+    def event_count(self, params={}):
+        return self._fetch_count(self._EVENTS, params)
+
+    def indicator_count(self, params={}):
+        return self._fetch_count(self._INDICATORS, params)
+
+    def pcap_count(self, params={}):
+        return self._fetch_count(self._PCAPS, params)
+
+    def raw_data_count(self, params={}):
+        return self._fetch_count(self._RAW_DATA, params)
+
+    def sample_count(self, params={}):
+        return self._fetch_count(self._SAMPLES, params)
+
+    def screenshot_count(self, params={}):
+        return self._fetch_count(self._SCREENSHOTS, params)
+
+    def target_count(self, params={}):
+        return self._fetch_count(self._TARGETS, params)
 
     def _fetch_binary(self, url, id_=None, params={}):
         params['username'] = self._username
